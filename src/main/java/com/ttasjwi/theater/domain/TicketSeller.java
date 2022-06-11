6ddac043bf8e9e7b36a7da3333b602373566582d
ttasjwi <ttasjwi920@gmail.com>
2022-06-11 10:01:58 +0900

    티켓 판매원 TicketSeller 정의
    - 자신이 일하는 매표소를 알고 있다.

---
+++ src/main/java/com/ttasjwi/theater/domain/TicketSeller.java
@@ -0,0 +1,14 @@
+package com.ttasjwi.theater.domain;
+
+public class TicketSeller {
+
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
