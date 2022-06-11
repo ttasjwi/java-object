9654e525901253a373b15130aa72a68a75178b2f
ttasjwi <ttasjwi920@gmail.com>
2022-06-11 10:18:37 +0900

    티켓 판매의 역할을 TicketSeller에 위임
    - 기존에는 Theater이 ticketSeller, ticketOffice을 알고 있었다. 이렇게 됐을 경우, 두가지 클래스의 변경이 일어나면 TicketSeller의 코드 변경도 발생하게 되는 문제가 있다.
    - TicketSeller의 getTicketOffice 메서드 제거. 더 이상 외부에서는 TicketSeller가 알고 있는 TicketOffice에 접근할 수 없게 된다. 객체 내부의 세부적인 사항을 감추고 객체간의 결합도를 낮춤.
    - 티켓을 판매하는 역할을 TicketSeller에게 위임하면 더 이상 Theater는 TicketOffice를 알고 있을 필요가 없다.

--- src/main/java/com/ttasjwi/theater/domain/Theater.java
+++ src/main/java/com/ttasjwi/theater/domain/Theater.java
@@ -9,15 +9,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            // 초대장을 확인하여 티켓을 건내준다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket(); // 티케 셀러에게 티켓을 얻어야하는데 티켓셀러가 알고있는 티켓 오피스까지 알아야하는 이상한 상황
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
--- src/main/java/com/ttasjwi/theater/domain/TicketSeller.java
+++ src/main/java/com/ttasjwi/theater/domain/TicketSeller.java
@@ -8,7 +8,17 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            // 초대장을 확인하여, 매표소에서 티켓을 건내준다.
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            // 초대장이 없으므로, 청중의 가방을 가져와(?) 요금을 징수하고 티켓을 전달한다.
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
