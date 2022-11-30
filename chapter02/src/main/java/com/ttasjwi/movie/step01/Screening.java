fe2184a1643539f55e56ad353e50805d4d7c739d
ttasjwi <ttasjwi920@gmail.com>
2022-11-30 10:03:02 +0900

    [chapter02] step1 - Screening에 영화 예매 기능 구현

--- chapter02/src/main/java/com/ttasjwi/movie/step01/Screening.java
+++ chapter02/src/main/java/com/ttasjwi/movie/step01/Screening.java
@@ -20,6 +20,14 @@ public class Screening {
         return whenScreened;
     }
 
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
     public boolean isSequence(int sequence) {
         return this.sequence == sequence;
     }
