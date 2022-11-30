36cc667877c5c646cb3b725941bb0979db794da6
ttasjwi <ttasjwi920@gmail.com>
2022-11-30 16:19:09 +0900

    [chapter02] step02 - 상속보다는 합성

--- chapter02/src/main/java/com/ttasjwi/movie/step02/Movie.java
+++ chapter02/src/main/java/com/ttasjwi/movie/step02/Movie.java
@@ -27,4 +27,14 @@ public class Movie {
         Money fee = this.fee.minus(discountAmount);
         return fee;
     }
+
+    /**
+     * 상속을 통해 할인 정책 로직을 구현하면, 컴파일 시점에 자식 클래스와 코드를 강하게 결합시켜버린다.
+     * DiscountPolicy 인터페이스를 의존하면 할인 정책 구현을 한 곳에 캡슐화하고, 할인 정책을 교체하기 쉬워진다.
+     *
+     * 코드의 재사용을 위해서는 상속보다는 합성을 선호하는 것이 더 좋은 방법이다.
+     */
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 }
