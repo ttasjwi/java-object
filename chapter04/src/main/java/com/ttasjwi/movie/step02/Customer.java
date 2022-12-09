8a8df9c1c3968afaa9c0105b76972366739a5002
ttasjwi <ttasjwi920@gmail.com>
2022-12-09 13:40:43 +0900

    [chapter04] step02 - step01 코드 복사

---
+++ chapter04/src/main/java/com/ttasjwi/movie/step02/Customer.java
@@ -0,0 +1,12 @@
+package com.ttasjwi.movie.step02;
+
+public class Customer {
+
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ chapter04/src/main/java/com/ttasjwi/movie/step02/Movie.java
@@ -0,0 +1,58 @@
+package com.ttasjwi.movie.step02;
+
+import com.ttasjwi.money.Money;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+
+    private String title;
+    private Duration runnintTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return discountConditions;
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+}
