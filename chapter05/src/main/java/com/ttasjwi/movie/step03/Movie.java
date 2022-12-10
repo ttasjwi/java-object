9825a686e8d8a1b81e7273f102a7014d60881b44
ttasjwi <ttasjwi920@gmail.com>
2022-12-10 16:34:24 +0900

    [chapter05] step03 - copy from step02

---
+++ chapter05/src/main/java/com/ttasjwi/movie/step03/Movie.java
@@ -0,0 +1,63 @@
+package com.ttasjwi.movie.step03;
+
+import com.ttasjwi.money.Money;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+
+    private final List<PeriodCondition> periodConditions = new ArrayList<>();
+    private final List<SequenceCondition> sequenceConditions = new ArrayList<>();
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money calculateMovieFee(Screening screening) {
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
+        return fee;
+    }
+
+    private boolean isDiscountable(Screening screening) {
+        return checkPeriodConditions(screening) || checkSequenceConditions(screening);
+    }
+
+    private boolean checkPeriodConditions(Screening screening) {
+        return sequenceConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private boolean checkSequenceConditions(Screening screening) {
+        return sequenceConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private Money calculateDiscountAmount() {
+        return switch (movieType) {
+            case AMOUNT_DISCOUNT -> calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT -> calculatePercentDiscountAmount();
+            case NONE_DISCOUNT -> calculateNoneDiscountAmount();
+        };
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
+}
