12deaded0a2d4b8fc515aaef2a735b62151a415e
ttasjwi <ttasjwi920@gmail.com>
2022-12-10 19:30:07 +0900

    [chapter05] step05 - 상영을 통해 '예약'함
    - 내부적으로 가격을 계산하는 로직들도 전부 '상영'이 책임진다.
    - '상영'이 내부적으로 예약을 생성하고 반환한다.

--- chapter05/src/main/java/com/ttasjwi/movie/step05/ReservationAgency.java
+++ chapter05/src/main/java/com/ttasjwi/movie/step05/ReservationAgency.java
@@ -1,12 +1,9 @@
 package com.ttasjwi.movie.step05;
 
-import com.ttasjwi.money.Money;
-
 public class ReservationAgency {
 
     public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-        Money fee = screening.calculateFee(audienceCount);
-        return new Reservation(customer, screening, fee, audienceCount);
+        return screening.reserve(customer, audienceCount);
     }
 
 }
--- chapter05/src/main/java/com/ttasjwi/movie/step05/Screening.java
+++ chapter05/src/main/java/com/ttasjwi/movie/step05/Screening.java
@@ -10,28 +10,20 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
-    public Money calculateFee(int audienceCount) {
+    public Reservation reserve(Customer customer, int audienceCount) {
+        Money fee = calculateFee(audienceCount);
+        return new Reservation(customer, this, fee, audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
         return movie.calculateMovieFee(this).times(audienceCount);
-    }
-
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
-        this.movie = movie;
     }
 
     public int getSequence() {
         return sequence;
     }
 
-    public void setSequence(int sequence) {
-        this.sequence = sequence;
-    }
-
     public LocalDateTime getWhenScreened() {
         return whenScreened;
     }
-
 }
