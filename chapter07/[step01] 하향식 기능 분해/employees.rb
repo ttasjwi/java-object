68ea99dee70d914f1fdd0b177d7103c172d51421
ttasjwi <ttasjwi920@gmail.com>
2022-12-14 16:14:08 +0900

    [chapter07] step01 - 하향식 기능분해

---
+++ chapter07/[step01] 하향식 기능 분해/employees.rb
@@ -0,0 +1,29 @@
+#encoding: UTF-8
+$employees = ["직원A", "직원B", "직원C"] # 직원
+$basePays = [400, 300, 250] # 기본 급여
+
+def main(name)
+  taxRate = getTaxRate() # 세율 입력
+  pay = calculatePayFor(name, taxRate) # 최종 급여 계산
+  puts(describeResult(name, pay)) # 결과를 형식화하여 출력
+end
+
+# 세율을 입력하여 반환
+def getTaxRate()
+  print("세율을 입력하세요: ")
+  return gets().chomp().to_f()
+end
+
+# 급여 계산
+def calculatePayFor(name, taxRate)
+  index = $employees.index(name) # 직원의 인덱스 획득
+  basePay = $basePays[index] # 직원의 기본 급여 획득
+  return basePay - (basePay * taxRate) # 직원의 급여 계산 후 반환
+end
+
+# 직원의 급여 계산 결과를 문자열로 변환
+def describeResult(name, pay)
+  return "이름 : #{name}, 급여 : #{pay}"
+end
+
+main("직원A")
