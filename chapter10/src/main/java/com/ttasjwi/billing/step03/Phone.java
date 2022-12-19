01a8c399047d9a38ab9b00871d1000abd164c70a
ttasjwi <ttasjwi920@gmail.com>
2022-12-19 10:45:45 +0900

    [chapter10] billing:step03 - 한 클래스에 두고 enum으로 타입을 구분 -> 낮은 응집도, 높은 결합도

--- chapter10/src/main/java/com/ttasjwi/billing/step03/Phone.java
+++ chapter10/src/main/java/com/ttasjwi/billing/step03/Phone.java
@@ -8,12 +8,39 @@ import java.util.List;
 
 public class Phone {
 
-    private Money amount; // 단위 요금
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    enum PhoneType {REGULAR, NIGHTLY}
+
+    private PhoneType type; // 요금제 타입
+
+    private Money amount; // 단위 요금 (일반 요금일 때)
+    private Money nightlyAmount; // 야간 단위 요금 (야간 요금제일 때)
+    private Money regularAmount; // 일반 단위 요금 (야간 요금제일 때)
     private Duration seconds; // 단위 시간
+
     private final List<Call> calls = new ArrayList<>();
 
+    /**
+     * 일반 요금제
+     */
     public Phone(Money amount, Duration seconds) {
+        this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
+    }
+
+    /**
+     * 야간 요금제
+     */
+    public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this(PhoneType.NIGHTLY, Money.ZERO, nightlyAmount, regularAmount, seconds);
+    }
+
+    public Phone(PhoneType type, Money amount, Money nightlyAmount,
+                 Money regularAmount, Duration seconds) {
+        this.type = type;
         this.amount = amount;
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
         this.seconds = seconds;
     }
 
@@ -37,9 +64,17 @@ public class Phone {
         Money result = Money.ZERO;
 
         for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            if (type == PhoneType.REGULAR) {
+                result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                    result = result.plus(
+                            nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                } else {
+                    result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                }
+            }
         }
-
         return result;
     }
 }
