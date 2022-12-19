4717011bd514be6533f83d243916eb301ba52f04
ttasjwi <ttasjwi920@gmail.com>
2022-12-19 13:43:16 +0900

    [chapter10] billing:step06 - 중복 코드를 부모 클래스로 올리기

--- chapter10/src/main/java/com/ttasjwi/billing/step06/NightlyDiscountPhone.java
+++ chapter10/src/main/java/com/ttasjwi/billing/step06/NightlyDiscountPhone.java
@@ -3,17 +3,14 @@ package com.ttasjwi.billing.step06;
 import com.ttasjwi.money.Money;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
-public class NightlyDiscountPhone {
+public class NightlyDiscountPhone extends AbstractPhone {
 
     private static final int LATE_NIGHT_HOUR = 22;
 
     private Money nightlyAmount;
     private Money regularAmount;
     private Duration seconds;
-    private final List<Call> calls = new ArrayList<>();
 
     public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
         this.nightlyAmount = nightlyAmount;
@@ -21,15 +18,7 @@ public class NightlyDiscountPhone {
         this.seconds = seconds;
     }
 
-    public Money calculateFee() {
-        Money result = Money.ZERO;
-        for (Call call : calls) {
-            result = result.plus(calculateCallFee(call));
-        }
-        return result;
-    }
-
-    private Money calculateCallFee(Call call) {
+    protected Money calculateCallFee(Call call) {
         if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
             return nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
         } else {
--- chapter10/src/main/java/com/ttasjwi/billing/step06/Phone.java
+++ chapter10/src/main/java/com/ttasjwi/billing/step06/Phone.java
@@ -3,47 +3,18 @@ package com.ttasjwi.billing.step06;
 import com.ttasjwi.money.Money;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Phone {
+public class Phone extends AbstractPhone {
 
     private Money amount; // 단위 요금
     private Duration seconds; // 단위 시간
-    private final List<Call> calls = new ArrayList<>();
 
     public Phone(Money amount, Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
     }
 
-    public void call(Call call) {
-        calls.add(call);
-    }
-
-    public List<Call> getCalls() {
-        return this.calls;
-    }
-
-    public Money getAmount() {
-        return amount;
-    }
-
-    public Duration getSeconds() {
-        return seconds;
-    }
-
-    public Money calculateFee() {
-        Money result = Money.ZERO;
-
-        for (Call call : calls) {
-            result = result.plus(calculateCallFee(call));
-        }
-
-        return result;
-    }
-
-    private Money calculateCallFee(Call call) {
+    protected Money calculateCallFee(Call call) {
         return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
     }
 }
