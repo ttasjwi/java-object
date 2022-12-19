d8ded4ece751a4366a1f3bae9dee7018c1f250bd
ttasjwi <ttasjwi920@gmail.com>
2022-12-19 12:56:00 +0900

    [chapter10] instrumented:step02 - copy from step01

---
+++ chapter10/src/main/java/com/ttasjwi/instrumented/step02/InstrumentedHashSet.java
@@ -0,0 +1,25 @@
+package com.ttasjwi.instrumented.step02;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class InstrumentedHashSet<E> extends HashSet<E> {
+
+    private int addCount;
+
+    @Override
+    public boolean add(E e) {
+        addCount ++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+}
---
+++ chapter10/src/test/java/com/ttasjwi/instrumented/step02/InstrumentedHashSetTest.java
@@ -0,0 +1,22 @@
+package com.ttasjwi.instrumented.step02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InstrumentedHashSetTest {
+
+    @Test
+    @DisplayName("상속으로 메서드 오버라이딩 오작용")
+    public void test() {
+        InstrumentedHashSet<String> languages = new InstrumentedHashSet<>();
+        languages.addAll(Arrays.asList("Java", "Ruby", "Scala"));
+
+        assertFalse(languages.getAddCount() == 3);
+        assertTrue(languages.getAddCount() == 6);
+    }
+}
