2852df007fe48574693606bd03ef4d405b87d60b
ttasjwi <ttasjwi920@gmail.com>
2022-12-19 13:52:52 +0900

    [chapter10] billing:step07 - copy from step06

---
+++ chapter10/src/main/java/com/ttasjwi/billing/step07/Phone.java
@@ -0,0 +1,20 @@
+package com.ttasjwi.billing.step07;
+
+import com.ttasjwi.money.Money;
+
+import java.time.Duration;
+
+public class Phone extends AbstractPhone {
+
+    private Money amount; // 단위 요금
+    private Duration seconds; // 단위 시간
+
+    public Phone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
