ead7b62da12a67109f1b1890f622610d4a5410af
ttasjwi <ttasjwi920@gmail.com>
2022-12-19 11:06:05 +0900

    [chapter10] billing:step04 - 상속을 이용한 코드 재사용
    - 문제점
      - 부모 클래스의 지식이 필요함.
      - 재사용을 위해 상속 계층 사이에 어떤 가정이 필요하게 될 것이며, 그 결과로 이해하기 어렵고 직관에 어긋나는 코드가 탄생
      - 자식 클래스 개발자는 부모 클래스의 구현 방법에 대한 정확한 지식을 가져야한다.
      - 상속은 결합도를 만들고, 부모-자식간 강결합이 코드를 수정하기 어렵게 만듬

--- chapter10/src/main/java/com/ttasjwi/billing/step04/NightlyDiscountPhone.java
+++ chapter10/src/main/java/com/ttasjwi/billing/step04/NightlyDiscountPhone.java
@@ -3,34 +3,31 @@ package com.ttasjwi.billing.step04;
 import com.ttasjwi.money.Money;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
-public class NightlyDiscountPhone {
+public class NightlyDiscountPhone extends Phone {
 
     private static final int LATE_NIGHT_HOUR = 22;
 
     private Money nightlyAmount;
-    private Money regularAmount;
-    private Duration seconds;
-    private final List<Call> calls = new ArrayList<>();
 
-    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+    public NightlyDiscountPhone(Money regularAmount, Money nightlyAmount, Duration seconds) {
+        super(regularAmount, seconds);
         this.nightlyAmount = nightlyAmount;
-        this.regularAmount = regularAmount;
-        this.seconds = seconds;
     }
 
+    @Override
     public Money calculateFee() {
-        Money result = Money.ZERO;
-        for (Call call : calls) {
+        Money result = super.calculateFee(); // 부모 클래스의 calculateFee 호출 -> 일반 요금을 적용했을 때의 요금
+        Money nightlyFee = Money.ZERO; // 야간요금으로 적용할 것을 기본 요금제로 계산했을 때 추가적으로 지불해야하는 금액
+
+        for (Call call : getCalls()) {
             if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-                result = result.plus(
-                        nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-            } else {
-                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                nightlyFee = nightlyFee.plus(
+                        getAmount().minus(nightlyAmount).times(
+                                call.getDuration().getSeconds() / getSeconds().getSeconds()));
             }
         }
+        result.minus(nightlyFee); // 야간 요금으로 할인해야할 가격을 차감
         return result;
     }
 }
