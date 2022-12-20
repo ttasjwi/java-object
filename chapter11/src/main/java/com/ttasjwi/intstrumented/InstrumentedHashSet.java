7bb43127c5ef7de1ea2c8bb031d0c742658ba14a
ttasjwi <ttasjwi920@gmail.com>
2022-12-20 12:54:45 +0900

    [chapter11] InstrumentedHashSet : 인터페이스를 상속하면서, 구체 인스턴스를 합성
    - Set 인터페이스를 실체화
    - HashSet을 내부에 합성
    - HashSet에 대한 구현 결합도는 제거하면서도, 퍼블릭 인터페이스는 그대로 유지
    - 포워딩 메서드 : 기존의 인터페이스를 그대로 유지하면서, 구현에 대한 결합 없이 일부 작동 방식을 변경하고 싶을 때 사용

---
+++ chapter11/src/main/java/com/ttasjwi/intstrumented/InstrumentedHashSet.java
@@ -0,0 +1,93 @@
+package com.ttasjwi.intstrumented;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class InstrumentedHashSet<E> implements Set<E> {
+
+    private final Set<E> set;
+    private int addCount = 0;
+
+    public InstrumentedHashSet(Set<E> set) {
+        this.set = set;
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount ++;
+        return set.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return set.addAll(c);
+    }
+
+    @Override
+    public int size() {
+        return set.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return set.contains(o);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return set.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return set.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return set.toArray(a);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return set.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return set.containsAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return set.retainAll(c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return set.removeAll(c);
+    }
+
+    @Override
+    public void clear() {
+        set.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return set.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return set.hashCode();
+    }
+
+}
