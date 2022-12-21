884289042fe68456a6642741a9252eb606981731
ttasjwi <ttasjwi920@gmail.com>
2022-12-21 15:00:36 +0900

    [chapter12] java-grade:step02 - copy from step01

---
+++ chapter12/src/main/java/com/ttasjwi/lecture/step02/GradeLecture.java
@@ -0,0 +1,55 @@
+package com.ttasjwi.lecture.step02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GradeLecture extends Lecture {
+
+    private final List<Grade> grades = new ArrayList<>();
+
+    public GradeLecture(String title, int pass, List<Integer> scores, List<Grade> grades) {
+        super(title, pass, scores);
+        this.grades.addAll(grades);
+    }
+
+    @Override
+    public String evaluate() {
+        return super.evaluate() + ", " + gradesStatistics();
+    }
+
+    private String gradesStatistics() {
+        return grades.stream()
+                .map(grade -> format(grade))
+                .collect(Collectors.joining(" "));
+    }
+
+    private String format(Grade grade) {
+        return String.format("%s:%d", grade.getName(), gradeCount(grade));
+    }
+
+    private long gradeCount(Grade grade) {
+        return getScores().stream()
+                .filter(grade::include)
+                .count();
+    }
+
+    /**
+     * 등급 이름을 통해 해당 등급의 성적을 반환
+     */
+    public double average(String gradeName) {
+        return grades.stream()
+                .filter(grade -> grade.isName(gradeName))
+                .findFirst()
+                .map(this::gradeAverage)
+                .orElse(0d);
+    }
+
+    private double gradeAverage(Grade grade) {
+        return getScores().stream()
+                .filter(grade::include)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+    }
+}
