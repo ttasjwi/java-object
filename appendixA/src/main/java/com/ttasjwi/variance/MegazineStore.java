c6dfdbaa80e2a66ee9a18a08f8396174b9a215dc
ttasjwi <ttasjwi920@gmail.com>
2022-12-27 15:47:23 +0900

    [chapter14] java:variance - 파라미터 타입 반공변성
    - 서브 클래스에서 파라미터 타입의 사전 조건을 더 약화시킬 수 있음.
    - 따라서 파라미터 타입으로 부모 클래스에서의 파라미터의 슈퍼타입으로 대체할 수 있다.
    - 하지만 자바에서는 파라미터 타입 반공변성을 지원하지 않는다.

--- appendixA/src/main/java/com/ttasjwi/variance/MegazineStore.java
+++ appendixA/src/main/java/com/ttasjwi/variance/MegazineStore.java
@@ -2,6 +2,16 @@ package com.ttasjwi.variance;
 
 public class MegazineStore extends BookStall {
 
+
+    //public Book sell(Publisher publisher) {
+    // 서브타입의 메서드에서 파라미터 타입이 슈퍼타입으로 되어 있더라도 실제 런타임에는 슈퍼타입이 주입될 것이며, 협력은 정상적으로 돌아간다.
+
+    // 파라미터 타입 반공변성 : 부모 클래스에서 구현된 메서드를 자식 클래스에서 오버라이딩할 때 파라미터 타입을 부모 클래스에서 사용한 파라미터의
+    // 슈퍼타입으로 대체할 수 있는 특성
+    // 더 약한 사전조건을 정의하는 것과 같으므로 계약에 의한 설계 관점에서 계약을 위반하지 않고, 리스코프 치환 원칙을 위배하지 않는다.
+    // 하지만 자바에서는 파라미터 타입 반공변성을 지원하지 않는다.
+
+
     @Override
     public Book sell(IndependentPublisher independentPublisher) {
         // 리턴타입 공변성 : 부모 클래스에서 구현된 메서드를 자식 클래스에서 오버라이딩할 때,
